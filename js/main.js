327ca04abfbbb53db72ba085079676ca6311a3fd
edisonorozco <edison.orozco@outlook.com>
2021-06-14 18:12:54 -0500

    Correccion muestra imagenes portafolio responsive

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", () => {
   const overlay = document.getElementById("overlay");
-  document.querySelectorAll(".main .portafolio .container .container-text-portafolio .container-portafolio .item img").forEach((element) => {
-    element.addEventListener("click", () => {
-      const path = element.getAttribute("src");
-      const description = element.parentNode.dataset.description;
-      console.log(description);
-      overlay.classList.add("active");
-      document.querySelector("#overlay img").src = path;
-      document.querySelector("#overlay .description").innerHTML = description;
+  document
+    .querySelectorAll(
+      ".main .portafolio .container .container-text-portafolio .container-portafolio .item img"
+    )
+    .forEach((element) => {
+      element.addEventListener("click", () => {
+        const path = element.getAttribute("src");
+        const description = element.parentNode.dataset.description;
+        overlay.classList.add("active");
+        document.querySelector("#overlay img").src = path;
+        document.querySelector("#overlay .description").innerHTML = description;
+      });
     });
-  });
 
   document.querySelector("#btn-close-image").addEventListener("click", () => {
     overlay.classList.remove("active");
