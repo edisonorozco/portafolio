2e395b257ca9b772600c042297bb5e11ad2ebe27
edisonorozco <edison.orozco@outlook.com>
2021-06-21 20:53:19 -0500

    se agregan validaciones al formulario de contacto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const btnSendEmail = document.querySelector("#send-button");
 const formContact = document.querySelector("#form-contact-now");
+const urlEmail =
+  "https://back-portafolio-web.herokuapp.com/api/email/contact-me-now";
 
 window.addEventListener("load", () => {
   const overlay = document.getElementById("overlay");
@@ -26,33 +28,94 @@ window.addEventListener("load", () => {
   });
 });
 
-btnSendEmail.addEventListener("click", (event) => sendEmail(event));
+btnSendEmail.addEventListener("click", (event) => btnSendEmailClick(event));
 
-function sendEmail(event) {
-
+function btnSendEmailClick(event) {
   event.preventDefault();
-
   let emailDto = getEmailDTOFromFrom();
+  sendEmail(emailDto);
+}
 
-  fetch("https://back-portafolio-web.herokuapp.com/api/email/contact-me-now", {
+function sendEmail(emailDto) {
+  const settings = {
     method: "POST",
+    timeout: 6000,
     body: JSON.stringify(emailDto),
     headers: {
       "Content-Type": "application/json",
     },
-  })
-    .then((response) => console.log(response))
-    .catch((error) => console.error("Error:", error))
-    .then((response) => alert('Correo enviado con exito !'));
+  };
+
+  fetch(urlEmail, settings)
+    .then((response) => response.json())
+    .then(() => alert("Email enviado con exito"))
+    .catch((err) => console.log(err));
 }
 
 function getEmailDTOFromFrom() {
-  let formContactData = new FormData(formContact);
+  const nameClient = document.getElementById("txtName").value.trim();
+  const emailClient = document.getElementById("txtEmail").value.trim();
+  const subject = document.getElementById("subject").value.trim();
+  const message = document.getElementById("message").value.trim();
 
-  let nameClient = formContactData.get("name-contact");
-  let emailClient = formContactData.get("email-contact");
-  let subject = formContactData.get("subject");
-  let message = formContactData.get("message");
+  if (nameClient === "") {
+    setErrorFrom(
+      document.getElementById("txtName"),
+      "Por favor ingrese un nombre !"
+    );
+  } else {
+    setSuccessFrom(document.getElementById("txtName"));
+  }
+
+  if (emailClient === "") {
+    setErrorFrom(
+      document.getElementById("txtEmail"),
+      "Por favor ingrese un email !"
+    );
+  } else if (!isEmil(emailClient)) {
+    setErrorFrom(
+      document.getElementById("txtEmail"),
+      "Por favor ingrese un email valido!"
+    );
+  } else {
+    setSuccessFrom(document.getElementById("txtEmail"));
+  }
+
+  if (subject === "") {
+    setErrorFrom(
+      document.getElementById("subject"),
+      "Por favor ingrese un asunto !"
+    );
+  } else {
+    setSuccessFrom(document.getElementById("subject"));
+  }
+
+  if (message === "") {
+    setErrorFrom(
+      document.getElementById("message"),
+      "Por favor ingrese un mensaje !"
+    );
+  } else {
+    setSuccessFrom(document.getElementById("message"));
+  }
 
   return new EmailDTO(nameClient, emailClient, subject, message);
+}
+
+function setErrorFrom(input, message) {
+  const formControl = input.parentNode;
+  const small = formControl.querySelector("small");
+  small.innerHTML = message;
+  formControl.className = "form-control error";
+}
+
+function setSuccessFrom(input) {
+  const formControl = input.parentNode;
+  formControl.className = "form-control success";
+}
+
+function isEmil(email) {
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
